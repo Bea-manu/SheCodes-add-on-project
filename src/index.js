520798ee71ce16798676dd11c4f87a6c6352888f
Bea-manu <136429203+Bea-manu@users.noreply.github.com>
2025-03-31 21:45:41 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 function displayRecipe(response){
     new Typewriter("#recipe",{
-        strings: true,
+        strings: response.data.answer,
+        autoStart: true,
         delay: 1,
         cursor:"",
     });
@@ -11,9 +12,12 @@ function generateRecipe(event){
 let instructionsInput = document.querySelector("#search-input");
 if (instructionsInput){
 let apiKey = "8f0ab40o57b12e2t3b934b3b4137cfbc";
-let context = "please write the recipe with ingredients and cooking steps";
+let context = `please write the recipe with ingredients and cooking steps`;
 let prompt = `User intructions: Generate full recipe ${instructionsInput.value}`;
 let apiUrl = `https://api.shecodes.io/recipes/search?query=${instructionsInput.value}&key=${apiKey}`;
 }
 axios.get(apiUrl).then(displayRecipe);
 }
+
+let recipeElement = document.querySelector("#search-form");
+recipeElement.addEventListener("submit", generateRecipe);
