5aa3e5a90074c3a42d7557842d7cd4036a649fe3
Bea-manu <136429203+Bea-manu@users.noreply.github.com>
2025-03-31 20:58:01 +0200

    update in html and js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
+function displayRecipe(response){
+    new Typewriter("#recipe"{
+        strings: true,
+        delay: 1,
+        cursor:"",
+    });
+}
 function generateRecipe(event){
     event.preventDefault();
 
-
-let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+let instructionsInput = document.querySelector("#search-input");
 let apiKey = "8f0ab40o57b12e2t3b934b3b4137cfbc";
-let context = ""
+let context = "please write the recipe with ingredients and cooking steps";
 let prompt = `User intructions: Generate full recipe ${instructionsInput.value}`;
+let apiUrl = `https://api.shecodes.io/recipes/search?query=${instructionsInput.value}&key=${apiKey}`;
+
+axios.get(apiUrl).then(displayRecipe);
 }
