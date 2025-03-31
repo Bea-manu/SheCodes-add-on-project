87081b8be2752b20ed714e01bdf6cf30bea5b8dc
Bea-manu <136429203+Bea-manu@users.noreply.github.com>
2025-03-31 21:19:08 +0200

    update in html and js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 function displayRecipe(response){
-    new Typewriter("#recipe"{
+    new Typewriter("#recipe",{
         strings: true,
         delay: 1,
         cursor:"",
@@ -9,10 +9,11 @@ function generateRecipe(event){
     event.preventDefault();
 
 let instructionsInput = document.querySelector("#search-input");
+if (instructionsInput){
 let apiKey = "8f0ab40o57b12e2t3b934b3b4137cfbc";
 let context = "please write the recipe with ingredients and cooking steps";
 let prompt = `User intructions: Generate full recipe ${instructionsInput.value}`;
 let apiUrl = `https://api.shecodes.io/recipes/search?query=${instructionsInput.value}&key=${apiKey}`;
-
+}
 axios.get(apiUrl).then(displayRecipe);
 }
